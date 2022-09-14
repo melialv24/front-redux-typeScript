08494178fabc806dab1a68b5923795f91b03166e
Melissa <malvarez@datagas.com.co>
2022-09-14 17:26:25 -0500

    agregando provider

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,13 +1,29 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "../hooks/useAuthStore";
 import { AuthLoginV } from "../Views";
+import { useEffect } from "react";
 import { AuthRegisterV } from "../Views/auth";
 
 export const AppRouter = () => {
+  const { status, checkAuthToken } = useAuthStore();
+
+  useEffect(() => {
+    checkAuthToken();
+  }, []);
+
   return (
     <Routes>
-      <Route path="/auth/login" element={<AuthLoginV />} />
-      <Route path="/auth/register" element={<AuthRegisterV />} />
-      <Route path="/*" element={<Navigate to="/auth/login" />} />
+      {status !== "not-authenticated" ? (
+        <>
+          <Route path="/auth/login" element={<AuthLoginV />} />
+          <Route path="/auth/register" element={<AuthRegisterV />} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/auth/login" element={<AuthLoginV />} />
+        </>
+      )}
     </Routes>
   );
 };
